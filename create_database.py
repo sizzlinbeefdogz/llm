75b4120ee27aacda6e48c3d6012ba851732e7071
Farzam Imani <fimani@ucdavis.edu>
2024-05-18 01:48:27 -0700

    shit mang fuck

--- create_database.py
+++ create_database.py
@@ -1,8 +1,8 @@
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-# from langchain_openai import OpenAIEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from langchain_community.vectorstores import Chroma
 import os
 import shutil
@@ -12,49 +12,42 @@ DATA_PATH = "data"
 
 
 def main():
-    generate_data_store()
+    documents = load_documents()    
+    save_to_chroma()
+    return
 
-
-def generate_data_store():
-    documents = load_documents()
-    chunks = split_text(documents)
-    save_to_chroma(chunks)
-
-
+# Load in data for tokenizer
 def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="*.txt")
     documents = loader.load()
     return documents
 
-
-def split_text(documents: list[Document]):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
-        length_function=len,
-        add_start_index=True,
-    )
-    chunks = text_splitter.split_documents(documents)
-    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-    
-    # document = chunks[10]
-    # print(document.page_content)
-    # print(document.metadata)
-
-    return chunks
-
-
-def save_to_chroma(chunks: list[Document]):
+def save_to_chroma():
     # Clear out the database first.
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
+    
+    # Load the pre-trained tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B")
+    
+    #Read text file into string
+    with open('data/test.txt', 'r') as file:
+        text = file.read()
+
+     # Tokenize the text data
+    tokenized_text = tokenizer.encode(text)
+
+    # Generate embeddings using the pre-trained model
+    embeddings = model(input_ids=tokenized_text['input_ids'], attention_mask=tokenized_text['attention_mask'])
+
 
     # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks, HuggingFaceEmbeddings(), persist_directory=CHROMA_PATH
+        tokenized_text, embeddings, persist_directory=CHROMA_PATH
     )
     db.persist()
-    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+    print(f"Saved {len(tokenized_text)} chunks to {CHROMA_PATH}.")
 
 
 if __name__ == "__main__":
